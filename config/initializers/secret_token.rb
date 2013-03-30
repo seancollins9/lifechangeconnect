220c6465476d60669ea814ab85d24263d67858b3
Sean Collins <seanfl@me.com>
2013-03-30 14:36:05 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lifechangeconnect::Application.config.secret_token = 'd6f512203ad7c2d87c7caf716b46d61b006222c765aaf80673da463f0b193b65885d0da75794568bc7503aa1fd9d30210ad3814825d749ac8ca55ff7ce93d439'
